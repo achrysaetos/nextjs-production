0b1e96cfad04ffd63e32869799087d02c0917502
achrysaetos <achrysaetos@yahoo.com>
2023-06-28 12:40:46 -0500

    make FAST

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,4 @@
 import Landing from '@/pages/public/landing';
-import { createServerSupabaseClient } from '@supabase/auth-helpers-nextjs';
-import { GetServerSidePropsContext } from 'next';
 
 export default function Home() {
   return <Landing />;
